4db801b603b37071e3d9bdba278d858b15ef563b
HaoSususudio <bluehao85@gmail.com>
2019-12-06 00:09:25 -0500

    improved USER_ROLE memory leak

--- src/main/java/com/revature/model/AppUser.java
+++ src/main/java/com/revature/model/AppUser.java
@@ -1,10 +1,8 @@
 package com.revature.model;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -21,176 +19,183 @@ import org.springframework.security.core.userdetails.UserDetails;
 @Table(name = "app_user")
 public class AppUser implements UserDetails {
 
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-	@Id
-	@Column(name = "username")
-	private String username;
+  @Id
+  @Column(name = "username")
+  private String username;
 
-	@Column(name = "password")
-	private String password;
+  @Column(name = "password")
+  private String password;
 
-	@Column(name = "enabled")
-	private boolean enabled;
+  @Column(name = "enabled")
+  private boolean enabled;
 
-	@Column(name = "accountNonExpired")
-	private boolean accountNonExpired;
+  @Column(name = "accountNonExpired")
+  private boolean accountNonExpired;
 
-	@Column(name = "accountNonLocked")
-	private boolean accountNonLocked;
+  @Column(name = "accountNonLocked")
+  private boolean accountNonLocked;
 
-	@Column(name = "credentialsNonExpired")
-	private boolean credentialsNonExpired;
+  @Column(name = "credentialsNonExpired")
+  private boolean credentialsNonExpired;
 
-	@OneToMany(mappedBy = "user", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-	private Set<ApplicationAuthority> authorities;
+  @OneToMany(mappedBy = "user", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+  private Set<ApplicationAuthority> authorities;
 
-	@OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-	private List<Todo> todos;
+  @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+  private List<Todo> todos;
 
-	@OneToOne(mappedBy = "user", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-	private UserInformation userInformation;
+  @OneToOne(mappedBy = "user", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+  private UserInformation userInformation;
 
-	@OneToMany(mappedBy = "createdBy", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-	private List<Message> messages;
+  @OneToMany(mappedBy = "createdBy", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+  private List<Message> messages;
 
-	public AppUser() {
-	}
+  public AppUser() {
+  }
 
-	public AppUser(String username) {
-		this.username = username;
-	}
+  public AppUser(String username) {
+    this.username = username;
+  }
 
-	public String getUsername() {
-		return username;
-	}
+  public String getUsername() {
+    return username;
+  }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+  public void setUsername(String username) {
+    this.username = username;
+  }
 
-	public String getPassword() {
-		return password;
-	}
+  public String getPassword() {
+    return password;
+  }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+  public void setPassword(String password) {
+    this.password = password;
+  }
 
-	public boolean isEnabled() {
-		return enabled;
-	}
+  public boolean isEnabled() {
+    return enabled;
+  }
 
-	public void setEnabled(boolean enabled) {
-		this.enabled = enabled;
-	}
+  public void setEnabled(boolean enabled) {
+    this.enabled = enabled;
+  }
 
-	public boolean isAccountNonExpired() {
-		return accountNonExpired;
-	}
+  public boolean isAccountNonExpired() {
+    return accountNonExpired;
+  }
 
-	public void setAccountNonExpired(boolean accountNonExpired) {
-		this.accountNonExpired = accountNonExpired;
-	}
+  public void setAccountNonExpired(boolean accountNonExpired) {
+    this.accountNonExpired = accountNonExpired;
+  }
 
-	public boolean isAccountNonLocked() {
-		return accountNonLocked;
-	}
+  public boolean isAccountNonLocked() {
+    return accountNonLocked;
+  }
 
-	public void setAccountNonLocked(boolean accountNonLocked) {
-		this.accountNonLocked = accountNonLocked;
-	}
+  public void setAccountNonLocked(boolean accountNonLocked) {
+    this.accountNonLocked = accountNonLocked;
+  }
 
-	public boolean isCredentialsNonExpired() {
-		return credentialsNonExpired;
-	}
+  public boolean isCredentialsNonExpired() {
+    return credentialsNonExpired;
+  }
 
-	public void setCredentialsNonExpired(boolean credentialsNonExpired) {
-		this.credentialsNonExpired = credentialsNonExpired;
-	}
+  public void setCredentialsNonExpired(boolean credentialsNonExpired) {
+    this.credentialsNonExpired = credentialsNonExpired;
+  }
 
-	public Set<ApplicationAuthority> getAuthorities() {
-		return authorities;
-	}
+  public Set<ApplicationAuthority> getAuthorities() {
+    return authorities;
+  }
 
-	public void setAuthorities(Collection<ApplicationAuthority> authorities) {
-		this.authorities = authorities.stream().collect(Collectors.toSet());
-		for (ApplicationAuthority authority : authorities) {
-			authority.setUser(this);
-		}
-	}
 
-	public List<Todo> getTodos() {
-		return todos;
-	}
+  /* @author: Hao Shen
+   * authorities is a SET. No need to declare it as Collection in the input then
+   * convert it back as SET.
+   * Actually, no need to over load this function at all.
+   */
+//	public void setAuthorities(Collection<ApplicationAuthority> authorities) {	  
+//	  this.authorities = authorities.stream().collect(Collectors.toSet());
+//		for (ApplicationAuthority authority : authorities) {
+//			authority.setUser(this);
+//		}
+//	}
 
-	public void setTodos(List<Todo> todos) {
-		this.todos = todos;
-	}
+  public List<Todo> getTodos() {
+    return todos;
+  }
 
-	public UserInformation getUserInformation() {
-		return userInformation;
-	}
+  public void setTodos(List<Todo> todos) {
+    this.todos = todos;
+  }
 
-	public void setUserInformation(UserInformation userInformation) {
-		this.userInformation = userInformation;
-	}
+  public UserInformation getUserInformation() {
+    return userInformation;
+  }
 
-	public void setAuthorities(Set<ApplicationAuthority> authorities) {
-		this.authorities = authorities;
-	}
+  public void setUserInformation(UserInformation userInformation) {
+    this.userInformation = userInformation;
+  }
 
-	public List<Message> getMessages() {
-		return messages;
-	}
+  public void setAuthorities(Set<ApplicationAuthority> authorities) {
+    this.authorities = authorities;
+  }
 
-	public void setMessages(List<Message> messages) {
-		this.messages = messages;
-	}
+  public List<Message> getMessages() {
+    return messages;
+  }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(accountNonExpired, accountNonLocked, authorities, credentialsNonExpired, enabled, password,
-				username);
-	}
+  public void setMessages(List<Message> messages) {
+    this.messages = messages;
+  }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (!(obj instanceof AppUser)) {
-			return false;
-		}
-		AppUser other = (AppUser) obj;
-		return accountNonExpired == other.accountNonExpired && accountNonLocked == other.accountNonLocked
-				&& Objects.equals(authorities, other.authorities)
-				&& credentialsNonExpired == other.credentialsNonExpired && enabled == other.enabled
-				&& Objects.equals(password, other.password) && Objects.equals(username, other.username);
-	}
+  @Override
+  public int hashCode() {
+    return Objects.hash(accountNonExpired, accountNonLocked, authorities, credentialsNonExpired,
+        enabled, password, username);
+  }
 
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("AppUser [username=");
-		builder.append(username);
-		builder.append(", password=");
-		builder.append(password);
-		builder.append(", enabled=");
-		builder.append(enabled);
-		builder.append(", accountNonExpired=");
-		builder.append(accountNonExpired);
-		builder.append(", accountNonLocked=");
-		builder.append(accountNonLocked);
-		builder.append(", credentialsNonExpired=");
-		builder.append(credentialsNonExpired);
-		builder.append(", authorities=");
-		builder.append(authorities);
-		builder.append("]");
-		return builder.toString();
-	}
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (!(obj instanceof AppUser)) {
+      return false;
+    }
+    AppUser other = (AppUser) obj;
+    return accountNonExpired == other.accountNonExpired
+        && accountNonLocked == other.accountNonLocked
+        && Objects.equals(authorities, other.authorities)
+        && credentialsNonExpired == other.credentialsNonExpired && enabled == other.enabled
+        && Objects.equals(password, other.password) && Objects.equals(username, other.username);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder builder = new StringBuilder();
+    builder.append("AppUser [username=");
+    builder.append(username);
+    builder.append(", password=");
+    builder.append(password);
+    builder.append(", enabled=");
+    builder.append(enabled);
+    builder.append(", accountNonExpired=");
+    builder.append(accountNonExpired);
+    builder.append(", accountNonLocked=");
+    builder.append(accountNonLocked);
+    builder.append(", credentialsNonExpired=");
+    builder.append(credentialsNonExpired);
+    builder.append(", authorities=");
+    builder.append(authorities);
+    builder.append("]");
+    return builder.toString();
+  }
 
 }
--- src/main/java/com/revature/service/RegistrationServiceImpl.java
+++ src/main/java/com/revature/service/RegistrationServiceImpl.java
@@ -1,58 +1,68 @@
 package com.revature.service;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
 import com.revature.model.AppUser;
 import com.revature.model.ApplicationAuthority;
 import com.revature.model.RegistrationForm;
 import com.revature.model.UserInformation;
 import com.revature.repository.UserInfoRepository;
 import com.revature.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-
-import java.util.Arrays;
 
 @Service
 public class RegistrationServiceImpl implements RegistrationService {
+  private final UserRepository repo;
+  private final UserInfoRepository infoRepo;
+  private final PasswordEncoder passwordEncoder;
 
-	private final UserRepository repo;
-	private final UserInfoRepository infoRepo;
-	private final PasswordEncoder passwordEncoder;
-	
-	@Autowired
-	public RegistrationServiceImpl(final UserRepository repo, final UserInfoRepository infoRepo, final PasswordEncoder passwordEncoder) {
-		this.repo = repo;
-		this.infoRepo = infoRepo;
-		this.passwordEncoder = passwordEncoder;
-	}
+  @Autowired
+  public RegistrationServiceImpl(final UserRepository repo, final UserInfoRepository infoRepo,
+      final PasswordEncoder passwordEncoder) {
+    this.repo = repo;
+    this.infoRepo = infoRepo;
+    this.passwordEncoder = passwordEncoder;
+  }
 
-	@Override
-	public AppUser attemptRegisterUser(RegistrationForm form) {
-		AppUser user = new AppUser();
-		user.setUsername(form.getUsername());
-		user.setPassword(passwordEncoder.encode(form.getPassword()));
-		user.setEnabled(true);
-		user.setAccountNonExpired(true);
-		user.setAccountNonLocked(true);
-		user.setCredentialsNonExpired(true);
-		user.setAuthorities(Arrays.asList(new ApplicationAuthority("ROLE_USER")));
-		
-		UserInformation info = new UserInformation();
-		info.setFirstname(form.getFirstname());
-		info.setLastname(form.getLastname());
-		info.setEmail(form.getEmail());
-		info.setPhoneNumber(form.getPhoneNumber());
-		info.setPhoneType(form.getPhoneType());
-		info.setCity(form.getCity());
-		info.setStreet(form.getStreet());
-		info.setState(form.getState());
-		info.setZipCode(form.getZipCode());
-		
-		info.setUser(user);
-		user.setUserInformation(info);
-		repo.save(user);
-		infoRepo.save(info);
-		return user;
-	}
+  @Override
+  public AppUser attemptRegisterUser(RegistrationForm form) {
+    final ApplicationAuthority userAuthority = new ApplicationAuthority("ROLE_USER");
+    final ApplicationAuthority[] newUserAuthoritiesValues = new ApplicationAuthority[] {
+        userAuthority };
+    final Set<ApplicationAuthority> newUserAuthorities = new HashSet<ApplicationAuthority>(
+        Arrays.asList(newUserAuthoritiesValues));
+
+    AppUser user = new AppUser();
+
+    user.setUsername(form.getUsername());
+    user.setPassword(passwordEncoder.encode(form.getPassword()));
+    user.setEnabled(true);
+    user.setAccountNonExpired(true);
+    user.setAccountNonLocked(true);
+    user.setCredentialsNonExpired(true);
+    user.setAuthorities(newUserAuthorities);
+
+    UserInformation info = new UserInformation();
+    info.setFirstname(form.getFirstname());
+    info.setLastname(form.getLastname());
+    info.setEmail(form.getEmail());
+    info.setPhoneNumber(form.getPhoneNumber());
+    info.setPhoneType(form.getPhoneType());
+    info.setCity(form.getCity());
+    info.setStreet(form.getStreet());
+    info.setState(form.getState());
+    info.setZipCode(form.getZipCode());
+
+    info.setUser(user);
+    user.setUserInformation(info);
+    repo.save(user);
+    infoRepo.save(info);
+    return user;
+  }
 
 }
